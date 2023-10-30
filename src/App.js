96abeefc564ba9b0fd73c242672fc151c1d96b6c
Antonina Graur <124353835+AntoninaGraur@users.noreply.github.com>
2023-10-30 11:33:18 +0100

    make html sceleton

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <section className='hero'>
+      <div className='content' >
+        <h1>З днем народження!!!</h1>
+        <a href='#'> Ти іменинниця сьогодні?</a>
+      </div>
+    </section>
+   
   );
 }
 
