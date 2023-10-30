4c1b3e49f6fec77119c31cf2a45626bb56c709ba
Antonina Graur <124353835+AntoninaGraur@users.noreply.github.com>
2023-10-30 12:47:52 +0100

    add button

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-
-import './App.css';
-import Image from './images/background2.jpg'
+import "./App.css";
+import Image from "./images/background2.jpg";
 
 function App() {
   return (
-    <section className='hero' style={{backgroundImage:`url(${Image})`}}>
-      <div className='content' >
-        <h1>З днем народження!!!</h1>
-        <a href='#'> Тицяй</a>
-      </div>
+    <section style={{opacity:"0.7"}}>
+      <div
+        className="hero"
+        style={{
+          backgroundImage: `url(${Image})`,
+        }}
+      >
+       
+      </div> <div className="content">
+        
+          <button type="submit" > Тицьни мене</button>
+        </div>
     </section>
-   
   );
 }
 
---
+++ src/album.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function album() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default album
