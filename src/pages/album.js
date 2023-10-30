a045cb40f3fd709f74e9c84a921880726b2d2c0c
Antonina Graur <124353835+AntoninaGraur@users.noreply.github.com>
2023-10-30 22:55:53 +0100

    not working data.json

--- src/pages/album.js
+++ src/pages/album.js
@@ -1,25 +1,35 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import jsonData from '../data.json';
+console.log(jsonData);
 
 function Album() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    axios.get('/src/data.json').then(response => {
-      setData(response.data)
-      
-    }).catch(err => {
-    console.log("Data loading error", err);
-  })
-}, [])
+    axios
+      .get("../data.json")
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((err) => {
+        console.log("Data loading error", err);
+      });
+  }, []);
 
   return (
     <div>
       <h1>My Album</h1>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>
+            <img src={item.image_url} alt="Personal foto" />
+            <p>{item.title}</p>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
 export default Album;
-
-
